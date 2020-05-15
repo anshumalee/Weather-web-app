6966008545c45d9a5e50e42851f02fc405e33830
Anshumalee <anshumalee1909@gmail.com>
2020-05-15 18:16:41 +0530

    Added humidity in forecast.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (latitude,longitude,callback) => {
             callback('Unable to find location',undefined)
         }
         else{
-            callback(undefined,body.current.weather_descriptions[0] + '.The temperature is ' + body.current.temperature + ' degrees. It feels like ' + body.current.feelslike + ' degrees.')
+            callback(undefined,body.current.weather_descriptions[0] + '.The temperature is ' + body.current.temperature + ' degrees. It feels like ' + body.current.feelslike + ' degrees. The humidity is '+ body.current.humidity + ' %.')
         }
     })
 }
