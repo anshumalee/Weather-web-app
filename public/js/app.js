be0cee5e6dadaaccf04a2ecbbeb3f2265c630cff
Anshumalee <anshumalee1909@gmail.com>
2020-05-15 17:07:12 +0530

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('Client side javascript is being loaded.')
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
